ac723416393aaebfbdf22940556f8ae1ef2ce29b
robertohastings <rob.hst@gmail.com>
2024-06-11 18:12:41 -0600

    update front y back

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const app = express()
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-const PORT = 5000
+//const PORT = 5000
 
 app.get("/api", (req, res) => {
-    res.send("Hola mundo from backend! 4")
+    res.send("Hola mundo from backend! 5")
 })
 
 app.listen(PORT, () => {
