3e682b3acf13eebdc9ccdada1d2d801e7591cfb6
robertohastings <rob.hst@gmail.com>
2024-06-13 18:30:58 -0600

    cambios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.js
+import React, { useEffect, useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import axios from "axios"
+
+//My Components
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Page from "./components/Page"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState("")
+
+    useEffect(() => {
+        axios
+            .get("/api")
+            .then(response => {
+                setData(response.data)
+            })
+            .catch(error => {
+                console.error("There was an error fetching the data!", error)
+            })
+    }, [])
+
+    return (
+        // <div className="App">
+        //     <header className="App-header">
+        //         <p>{data}</p>
+        //     </header>
+        // </div>
+
+        <BrowserRouter>
+            <Header />
+            <Page title="Home Page" />
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,25 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 import { Navbar, Nav } from "react-bootstrap"
 
 function Header() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="/">Responsive Web Template</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <div className="flex-row my-3 my-md-0">
+            <Navbar bg="dark" variant="dark" expand="lg">
+                {/* <Navbar.Brand href="/">Responsive Web Template</Navbar.Brand> */}
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#home">About Us</Nav.Link>
-                    <Nav.Link href="#ContactUs">Contact Us</Nav.Link>
-                    <Nav.Link href="#Products">Products</Nav.Link>
-                    <Nav.Link href="#Services">Services</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#home">About Us</Nav.Link>
+                        <Nav.Link href="#ContactUs">Contact Us</Nav.Link>
+                        <Nav.Link href="#Products">Products</Nav.Link>
+                        <Nav.Link href="#Services">Services</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
     )
 }
 
