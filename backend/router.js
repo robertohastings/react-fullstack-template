11ad2f63aebd1f988e7e9545911d8891d2654269
robertohastings <rob.hst@gmail.com>
2024-06-19 18:09:50 -0600

    updates email

--- backend/router.js
+++ backend/router.js
@@ -2,6 +2,8 @@ const express = require("express")
 const apiRouter = express.Router()
 
 const holaMundoController = require("./controllers/holaMundaController")
+const landingController = require("./controllers/landingController")
+const emailSendController = require("./controllers/emailSendController")
 
 const cors = require("cors")
 
@@ -9,5 +11,7 @@ apiRouter.use(cors())
 
 apiRouter.get("/router", (req, res) => res.json("Hello, if you see this message that means that backend routing is up and running successfully"))
 apiRouter.get("/holamundo", holaMundoController.apiHolaMundo)
+apiRouter.get("/landingPage", landingController.apiLanding)
+apiRouter.post("/send-email-by-gmail", emailSendController.apiSendEmailByGmal)
 
 module.exports = apiRouter
