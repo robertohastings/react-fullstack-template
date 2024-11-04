8a22b2eb786b404829a428ab507f986a723b17ec
robertohastings <rob.hst@gmail.com>
2024-11-04 15:56:09 -0600

    cambios backend

--- backend/config.js
+++ backend/config.js
@@ -11,7 +11,7 @@ config()
 // export const DB_PASSWORD = "@m.YFDS]c2rkU9MQ"
 
 // Local
-export const PORT = process.env.PORT || 8080
+export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || "104.200.137.56"
 export const DB_PORT = process.env.DB_PORT || 3306
 export const DB_DATABASE = process.env.DB_DATABASE || "crm"
