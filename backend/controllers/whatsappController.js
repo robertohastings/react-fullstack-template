04c495bf1d594f3752cb5b292048019d05cd2eb0
robertohastings <35307304+robertohastings@users.noreply.github.com>
2025-01-26 09:15:33 -0600

    jsonLog.js

--- backend/controllers/whatsappController.js
+++ backend/controllers/whatsappController.js
@@ -1,3 +1,16 @@
+//const fs = require('fs')
+//const myConsole = new console.Console(fs.createWriteStrem("./logs.txt"))
+
+import { response } from "express"
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from "url"
+import { appendToJsonLog } from "../helpers/jsonLog.js"
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export const VerifyToken = (req, res) => {
     
     try {
@@ -16,7 +29,41 @@ export const VerifyToken = (req, res) => {
     }
     //res.send("hola verifyToken")
 }
-
+//ctl k + M
+//TODO: CONOCER EL PHONE_NUMBER_ID DE LA APP PARA CREAR UN ARCHIVO JSON POR CADA UNO
 export const ReceivedMessage = (req, res) => {
-    res.send("Hola Received")
+    try {
+        var entry = (req.body["entry"])[0]
+        var changes = (entry["changes"])[0]
+        var value = changes["value"]
+        
+        var messageObject = JSON.stringify(value["messages"])
+
+        //var valueJSON = JSON.stringify(value)
+        //console.log(valueJSON)
+        const phone_number_Id = value.metadata.phone_number_Id
+
+        //console.log(phone_number_Id)
+
+
+        appendToJsonLog(messageObject, phone_number_Id)
+
+
+        //myConsole.log(messageObject)
+        var message = messageObject
+        const logFilePath = path.join(__dirname, "../", 'logs', 'whatsapp.log'); // Ruta al archivo de log
+        const timestamp = new Date().toISOString();
+      
+        fs.appendFile(logFilePath, `${timestamp} - ${message}\n`, (err) => {
+          if (err) {
+            console.error('Error al escribir en el archivo de log:', err);
+          }
+        });        
+
+        res.send("EVENT_RECEIVED")
+    } catch (error) {
+        console.log(error)
+        res.send("EVENT_RECEIVED")
+    }
+    //res.send("Hola Received")
 }
---
+++ backend/helpers/jsonLog.js
@@ -0,0 +1,62 @@
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from "url"
+import { convertirFecha } from "../helpers/fecha.js"
+
+export function appendToJsonLog(dataReceived, phone_number_Id) {
+
+    var newJsonData = crearNuevoJson(dataReceived)
+
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+
+    //const logFilePath = 'log.json';
+    // Ruta al archivo de log
+    const logFilePath = path.join(__dirname, "../", 'logs', `${phone_number_Id}_whatsapp.js`); 
+
+    // Leer el archivo existente (si existe)
+    fs.readFile(logFilePath, 'utf8', (err, data) => {
+        let logData = [];
+        if (err) {
+            if (err.code !== 'ENOENT') {
+                console.error('Error al leer el archivo de log:', err);
+                return;
+            }
+        }/* else {
+        
+            try {
+            logData = JSON.parse(data);
+            
+
+            } catch (error) {
+                console.error('Error al parsear el archivo de log:', error);
+            }
+        }
+        */
+        if (data) {
+            logData = JSON.parse(data)
+        } 
+        
+        // Agregar el nuevo registro
+        logData.push(newJsonData);
+
+        // Ordenar los registros por fecha (suponiendo que data.fecha existe)
+        logData.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+
+        // Escribir el archivo
+        fs.writeFile(logFilePath, JSON.stringify(logData, null, 2), (err) => {
+            if (err) {
+                console.error('Error al escribir en el archivo de log:', err);
+            }
+        });
+    });
+}
+
+function crearNuevoJson(data) {
+    const nuevoJson = {
+      timestamp: convertirFecha(new Date().toISOString()) ,
+      data: JSON.parse(data)
+    };
+    return nuevoJson;
+  }
